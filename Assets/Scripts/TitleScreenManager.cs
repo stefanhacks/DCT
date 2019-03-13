a3bd9a42f0b28d31eb98e94bf75925ed37a09773
Filipe Ghisi <filipe.ghisi@gmail.com>
2019-03-13 08:27:43 -0300

    Title Scene camera configuration and scripts for title menu flow.

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public GameObject RootTitle, RootTitleMenu, RootHighScores, RootBG;
+
+    public void Awake()
+    {
+        Invoke("ShowTitleMenu", 1);
+    }
+
+    public void ShowTitleMenu()
+    {
+        RootTitle.SetActive(true);
+        RootHighScores.SetActive(false);
+        RootTitleMenu.SetActive(true);
+    }
+
+    public void ShowHighScores()
+    {
+        RootTitle.SetActive(false);
+        RootTitleMenu.SetActive(false);
+        RootHighScores.SetActive(true);
+    }
+}
